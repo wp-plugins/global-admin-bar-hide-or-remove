f28acd693bce1a5ab3dba0cc1b92d5657fe7817c
slangjis <slangjis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 03:30:34 +0000

    Version 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/global-admin-bar-hide-or-remove/tags/1.6.1@916348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ header( 'Connection: Close' );
 <title>404 Not Found</title>
 <script type="text/javascript">if(parent.frames.length>0){window.top.location.href=location.href}</script>
 <meta name="robots" content="noindex,nofollow,noarchive,nocache,nopreview,nosnippet,notranslate,noimageindex,nomediaindex,novideoindex,noodp,noydir">
-<link rel="canonical" href="<?php $url="//".$_SERVER["HTTP_HOST"]; echo $url; ?>/">
+<link rel="canonical" href="<?php $url="//".$_SERVER["HTTP_HOST"];echo $url?>/">
 </head>
 <body>
 <h1>Not Found</h1>
